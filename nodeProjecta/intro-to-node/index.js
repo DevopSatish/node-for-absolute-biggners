7787e01cc5a8b03486e7faa09561b87c1ae1917d
SATISH V <75536323+DevopSatish@users.noreply.github.com>
2022-12-15 04:35:45 +0530

    first

---
+++ nodeProjecta/intro-to-node/index.js
@@ -0,0 +1,6 @@
+var superheros = require('superheroes');
+var supervillains =require('supervillains');
+var mySuperVillainsName=supervillains.random();
+var mySuperHeroName=superheros.random();
+console.log("superVillain: "+mySuperVillainsName);
+console.log("superHero: "+mySuperHeroName);
